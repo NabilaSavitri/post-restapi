e90e061a64a76f8e552eb2fb3cf6cf583ec31ee7
NabilaSavitri <nabilasavitri1206@mhs.mdp.ac.id>
2023-05-26 11:25:58 +0700

    save

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv/config')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("Hello World3!");
+  res.send("Hello Nabila!");
 });
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+let db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'error establishing a database connection?'))
+
+db.once('open', () => {
+  console.log('Database is connected')
+})
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Server running on port ${process.env.PORT}`);
+})
